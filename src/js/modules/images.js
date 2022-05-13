e9e47627fa68cb43a509dbfa5e4164fd3b7e77f0
Insa <ilya2002i52@mail.ru>
2022-05-13 10:08:29 +0500

    buildV1

---
+++ src/js/modules/images.js
@@ -0,0 +1,37 @@
+const images = () => {
+	const imgPopup = document.createElement("div"),
+		workSection = document.querySelector(".works"),
+		bigImage = document.createElement("img");
+
+	imgPopup.classList.add("popup");
+
+	workSection.appendChild(imgPopup);
+
+	bigImage.style.cssText = "max-width: 500px; width:100%; max-height: 500px";
+	imgPopup.style.justifyContent = "center";
+	imgPopup.style.alignItems = "center";
+	imgPopup.style.display = "none";
+
+	imgPopup.appendChild(bigImage);
+
+	workSection.addEventListener("click", (e) => {
+		e.preventDefault();
+		let target = e.target;
+
+		if (target && target.classList.contains("preview")) {
+			imgPopup.style.display = "flex";
+
+			const path = target.parentNode.getAttribute("href");
+
+			bigImage.setAttribute("src", path);
+			document.body.style.overflow = "hidden";
+		}
+
+		if (target && target.matches("div.popup")) {
+			imgPopup.style.display = "none";
+			document.body.style.overflow = "scroll";
+		}
+	});
+};
+
+export default images;
